b54766bd764b3c567eb808967836b98843d6a9a8
carolinabpineiro <carolinabpineiro@gmail.com>
2024-07-15 04:07:26 -0300

    Se modifican detalles en vista Details

--- movieStack/details.js
+++ movieStack/details.js
@@ -10,70 +10,72 @@ if(urlParams.has('id')){
   let movie = movies.find((movie) => movie.id == urlParams.get('id'))
 console.log(movie);
 verMas.innerHTML =`
-<div class="container mx-auto p-4 bg-white rounded-lg shadow-lg">
-    <div class="flex justify-between mb-4">
-      <h1 class="text-3xl font-bold text-gray-800">${movie.title}</h1>
+<div class="container mx-auto p-8 bg-white rounded-lg shadow-lg flex flex-col items-center">
+    <div class="mb-4 text-center">
+        <h1 class="text-3xl font-bold text-gray-800">${movie.title}</h1>
     </div>
-    <div class="flex flex-wrap gap-4">
-      <div class="flex flex-row w-full md:w-2/3">
-        <div class="flex flex-row space-y-4">
-          <img src="${movie.image}" alt="Imagen de la película" class="w-full h-80 object-cover rounded-lg mb-4">
+    <div class="flex flex-wrap gap-4 w-full">
+        <div class="w-full md:w-2/3 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 items-center">
+            <img src="${movie.image}" alt="Imagen de la película" class="w-full h-auto object-cover rounded-lg mb-4 md:w-1/2">
 
-          <div>
-            
-            <p class="text-gray-600 mb-2"><span claass="font-bold">Frase clave:</span> ${movie.tagline}</p>
-            <p class="text-gray-700 mb-4">${movie.genres}</p>
-            <p class="text-gray-700 mb-4">${movie.overview}</p>
-            <ul class="text-gray-700">
-            </ul>
-          </div>
-        </div>
-      </div>
 
-      <!-- Filas 1 y 2 (tablas) -->
-      <div class="w-full flex flex-row md:w-1/3">
-        <div class="mb-4">
-          <h3 class="text-lg font-bold text-gray-800 mb-2">Tabla 1</h3>
-          <table class="w-full border-collapse border border-gray-200">
-            <tbody class="text-gray-700">
-              <tr>
-                <td class="border border-gray-200 p-2">Fila 1, Columna 1</td>
-                <td class="border border-gray-200 p-2">Fila 1, Columna 2</td>
-              </tr>
-              <tr>
-                <td class="border border-gray-200 p-2">Fila 2, Columna 1</td>
-                <td class="border border-gray-200 p-2">Fila 2, Columna 2</td>
-              </tr>
-              <tr>
-                <td class="border border-gray-200 p-2">Fila 3, Columna 1</td>
-                <td class="border border-gray-200 p-2">Fila 3, Columna 2</td>
-              </tr>
-            </tbody>
-          </table>
+            <div class="flex-1 text-center md:text-left">
+                <h3 class="text-lg font-bold text-gray-800 mb-2">Descripción:</h3>
+                <p class="text-gray-600 mb-4">${movie.tagline}</p>
+                <h3 class="text-lg font-bold text-gray-800 mb-2">Géneros:</h3>
+                <p class="text-gray-700 mb-4">${movie.genres.join(", ")}</p>
+                <h3 class="text-lg font-bold text-gray-800 mb-2">Resumen:</h3>
+                <p class="text-gray-700 mb-4">${movie.overview}</p>
+            </div>
         </div>
 
-        <div>
-          <h3 class="text-lg font-bold text-gray-800 mb-2">Tabla 2</h3>
-          <table class="w-full border-collapse border border-gray-200">
-            <tbody class="text-gray-700">
-              <tr>
-                <td class="border border-gray-200 p-2">Fila 1, Columna 1</td>
-                <td class="border border-gray-200 p-2">Fila 1, Columna 2</td>
-              </tr>
-              <tr>
-                <td class="border border-gray-200 p-2">Fila 2, Columna 1</td>
-                <td class="border border-gray-200 p-2">Fila 2, Columna 2</td>
-              </tr>
-              <tr>
-                <td class="border border-gray-200 p-2">Fila 3, Columna 1</td>
-                <td class="border border-gray-200 p-2">Fila 3, Columna 2</td>
-              </tr>
-            </tbody>
-          </table>
+        <div class="w-full md:w-1/3 flex flex-col md:flex-row gap-4">
+            <div class="w-full mb-8 md:w-1/2">
+                <table class="w-full border-collapse border border-gray-200">
+                    <tbody class="text-gray-700">
+                        <tr>
+                            <td class="border border-gray-200 p-2">Lenguaje original</td>
+                            <td class="border border-gray-200 p-2">${movie.original_language}</td>
+                        </tr>
+                        <tr>
+                            <td class="border border-gray-200 p-2">Fecha de estreno</td>
+                            <td class="border border-gray-200 p-2">${movie.release_date}</td>
+                        </tr>
+                        <tr>
+                            <td class="border border-gray-200 p-2">Duración</td>
+                            <td class="border border-gray-200 p-2">${movie.runtime} min</td>
+                        </tr>
+                        <tr>
+                            <td class="border border-gray-200 p-2">Estado</td>
+                            <td class="border border-gray-200 p-2">${movie.status}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="w-full mb-8 md:w-1/2">
+                <table class="w-full border-collapse border border-gray-200">
+                    <tbody class="text-gray-700">
+                        <tr>
+                            <td class="border border-gray-200 p-2">Votos</td>
+                            <td class="border border-gray-200 p-2">${movie.vote_average}</td>
+                        </tr>
+                        <tr>
+                            <td class="border border-gray-200 p-2">Presupuesto</td>
+                            <td class="border border-gray-200 p-2">${movie.budget}</td>
+                        </tr>
+                        <tr>
+                            <td class="border border-gray-200 p-2">Ingresos generados</td>
+                            <td class="border border-gray-200 p-2">${movie.revenue}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+
+
 `
 }
 
